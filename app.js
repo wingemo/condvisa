7791d357b33e9a09ec726b48861b2d1c5380524f
Wingemo <philip.wingemo@outlook.com>
2022-11-24 20:40:55 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    socket.on('connect', (data) => {
+        socket.io.engine.id = data.id;
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    45});
+    socket.on('send', (data) => {
+        socket.to(data.id).emit(data.msg);
+    });
+    socket.on('acknowledge', (data) => {
+        socket.to(data.id).emit(data.msg);
+    });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
