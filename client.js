a01ab0dd52daba086fa62e2d9425a857e30eb70b
Wingemo <philip.wingemo@outlook.com>
2022-11-24 22:13:34 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 function connect() {
-  socket.emit('connect', input.phone);
+    socket.emit('connect', input.phone);
 }
 
 function ping(phones) {
-  for (String phone : phones) {
-     socket.emit('ping', phone);
-     socket.on("ping", () => {
-      console.log("connected")
-    });
-  }
+    for (String phone: phones) {
+        socket.emit('ping', phone);
+        socket.on("ping", () => {
+            console.log("connected")
+        });
+    }
 }
