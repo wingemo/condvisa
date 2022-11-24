dd57a25612992818cb4cf07208faa6013578acc7
Wingemo <philip.wingemo@outlook.com>
2022-11-24 22:10:57 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,3 +1,15 @@
+function connect() {
+  socket.emit('connect', input.phone);
+}
+
+function check(phones) {
+  for (String phone : phones) {
+     socket.emit('check', phone);
+  }
+}
+
+socket.emit('check', input.phone);
+
 socket.on("connect", () => {
   socket.emit("connect", phone);
 });
